c53de7b38e9e984b1f15a10e6fe213a9258314b1
subrata-bhunia <eerrcfgy@gmail.com>
2020-12-12 15:04:02 +0530

    done

--- src/Components/Carousal.jsx
+++ src/Components/Carousal.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Carousel from "react-bootstrap/Carousel";
-import Slide1 from '../Images/Carousel/slide1.jpg';
-import Slide2 from '../Images/Carousel/slide2.jpg';
-import Slide3 from '../Images/Carousel/slide3.jpg';
-import Slide4 from '../Images/Carousel/slide4.jpg';
+// import Slide1 from '../Images/Carousel/slide1.jpg';
+// import Slide2 from '../Images/Carousel/slide2.jpg';
+// import Slide3 from '../Images/Carousel/slide3.jpg';
+// import Slide4 from '../Images/Carousel/slide4.jpg';
 
 const  Carousal=()=>{
     return(
         <div className="mb-3">
-            <Carousel  slide={false} fade={true} controls={false} indicators interval={2500} pause={false} >
+            <Carousel  slide={false} fade={true} controls={false} indicators interval={50000} pause={false} >
                 <Carousel.Item className="aa">
-                    <img src={Slide1}  alt="......" className=" d-block custom-img"  />
+                    <img src="https://source.unsplash.com/600x900/?coding,computer"  alt="......" className=" d-block custom-img"  />
                 </Carousel.Item>
                 <Carousel.Item className="aa">
-                    <img src={Slide2}  alt="......" className=" d-block  custom-img"  />
+                    <img src={"https://source.unsplash.com/1600x900/?hacking,computer"}  alt="......" className=" d-block  custom-img"  />
                 </Carousel.Item>
                 <Carousel.Item className="aa">
-                    <img src={Slide3}  alt="......" className=" d-block  custom-img"  />
+                    <img src={"https://source.unsplash.com/daily?webdesign"}  alt="......" className=" d-block  custom-img"  />
                 </Carousel.Item>
                 <Carousel.Item className="aa">
-                    <img src={Slide4}  alt="......" className=" d-block custom-img"  />
+                    <img src={"https://source.unsplash.com/daily?hacking"}  alt="......" className=" d-block custom-img"  />
                 </Carousel.Item>
             </Carousel>
         </div>
