a663e8f5bed6eafd72e2920634931a1ab87c6845
subrata-bhunia <eerrcfgy@gmail.com>
2020-12-15 13:22:39 +0530

    Admin Dashboard design

--- src/Pages/Admin/AdminPanel.jsx
+++ src/Pages/Admin/AdminPanel.jsx
@@ -1,10 +1,126 @@
 import React from 'react';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle';
+// import logo from '../../logo.svg';
 
-
+const Demo=()=>{
+    console.log("Ok")
+}
 const AdminPanel=()=>{
+
     return(
-        <div className="text-center bold">
-            Admin Panel
+        <div className="admin-dash">
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1 bold">Dashboard</span>
+                    <span>(ChatBot Data) </span>
+                </div>
+            </nav>
+            <div className="responsive">
+            <div className="box-adminpanel-left">
+                <h1>New Questions & Answer Set </h1>
+                {/* Question Set */}
+                <form className="ques-div">
+                   <p>Questions</p> 
+                   {/* id */}
+                <div>
+                    <label>Question Id :</label>
+                    <br />
+                    <input 
+                     placeholder="Enter Question id"
+                    />
+                    <i onClick={Demo} className="fa fa-arrow-circle-right icon-view"></i>
+                </div>
+                   {/* messege */}
+                <div>
+                    <label>Question :</label>
+                    <br />
+                    <input 
+                     placeholder="Enter Question ..."
+                    />
+                    <i onClick={Demo} className="fa fa-arrow-circle-right icon-view"></i>
+                </div>
+                   {/* trigger */}
+                <div>
+                    <label>Answer id :</label>
+                    <br />
+                    <input 
+                     placeholder="Enter Answer id"
+                    />
+                    <i onClick={Demo} className="fa fa-arrow-circle-right icon-view"></i>
+                </div>
+                   {/* component */}
+                <div>
+                    <label>Components :</label>
+                    <br />
+                    <input 
+                     placeholder="Enter Components"
+                     disabled
+                    />
+                    <i onClick={Demo} className="fa fa-arrow-circle-right icon-view"></i>
+                </div>
+                   {/* end */}
+                <div>
+                    <label>End :</label>
+                    <br />
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="end" id="true" value="end_yes"/>
+                        <label className="form-check-label">Yes</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="end" id="false" value="end_no" defaultChecked/>
+                        <label className="form-check-label">No</label>
+                    </div>
+                </div>
+                   {/* user */}
+                <div>
+                    <label>User Input :</label>
+                    <br />
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="user" id="true" value="user_yes"/>
+                        <label className="form-check-label">Yes</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="user" id="false" value="user_no" defaultChecked/>
+                        <label className="form-check-label">No</label>
+                    </div>
+                    </div>
+                   {/* waitaction */}
+                <div>
+                    <label>Question Id :</label>
+                    <br />
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="true" value="waitaction_yes"/>
+                        <label className="form-check-label">Yes</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="false" value="waitaction_no" defaultChecked/>
+                        <label className="form-check-label">No</label>
+                    </div>
+                    </div>
+                    <div className="icon-view-submit">
+                        <i onClick={Demo} className="fa fa-arrow-circle-right icon-submit" type="submit"></i>
+                    </div>
+                </form>
+                {/* Answer Set */}
+                <div className="ques-div">
+                   <p>Answer</p> 
+                <div>
+                    <label>Question Id :</label>
+                    <br />
+                    <input 
+                     placeholder="Enter Question id"
+                    />
+                    <i onClick={Demo} className="fa fa-arrow-circle-right icon-view"></i>
+                </div>
+                </div>
+            </div>
+            <div className="box-adminpanel-right">
+            <h1>Setup Preview</h1>
+            <div className="setup-div">
+
+            </div>
+            </div>
+            </div>
         </div>
     )
 }
--- src/Pages/Admin/Login.jsx
+++ src/Pages/Admin/Login.jsx
@@ -1,19 +1,70 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+class Login extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      username:'',
+      password:'',
+      path:'/admin'
+    };
+    this.handleChange_user = this.handleChange_user.bind(this);
+    this.handleChange_password = this.handleChange_password.bind(this);
+    this.Checking = this.Checking.bind(this);
+  };
+  handleChange_user(event) {
+    this.setState({username: event.target.value});
+  }
+  handleChange_password(event) {
+    this.setState({password: event.target.value});
+  }
+  Checking(event) {
+    event.preventDefault();
+    if (this.state.username === "" || this.state.password === "") {
+      alert("Fields are required");
+      return;
+    }
+    if (this.state.username === "subrata" && this.state.password === "1234") {
+      this.props.history.push('/adminpanel')
+      return;
+    }else{
+      alert("Login Failed ! . Check Username and Password.")
+    }
+  }
 
-const Login=()=>{
+  render(){
   return(
     <div>
-      <form className="box-admin">
+      <form className="box-admin" onSubmit={this.Checking} action={this.state.path}>
+        <img src={logo} alt="........" />
         <h1>Login</h1>
-        <input type="text" name=" " placeholder="username/email/mobile" />
-        <input type="password" name=" " placeholder="Password"  />
-        <NavLink exact to="/adminpanel" className="text-decoration-none"><input type="submit" name=" " value="Login" /></NavLink>
+        <input
+           type="text" 
+           name="username " 
+           value={this.state.username}
+           onChange={this.handleChange_user}
+           placeholder="Username" 
+           id="username"
+           />
+
+        <input 
+         type="password" 
+         name="password" 
+         value={this.state.password}
+         onChange={this.handleChange_password}
+         placeholder="Password"  
+         id="password"
+         autoComplete="cc-number"
+         />
+        <input type="submit" name=" " value="Login"  />
+         {/* <NavLink exact to={this.state.path} className=" text-decoration-none">
+           <input type="submit" name=" " value="Login"  />
+         </NavLink> */}
       </form>
     </div>
   )
 }
-
+}
 
 
 export default Login;
